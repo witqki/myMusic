7c636b0a744210fa5879a247985a0efd94c8a31b
wiki <wiki@DESKTOP-JGU2RS3>
2019-04-03 20:52:22 +0800

    新增music的一些基础类

---
+++ myMusic/src/main/java/com/example/myMusic/music/entities/Music.java
@@ -0,0 +1,12 @@
+package com.example.myMusic.music.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.example.myMusic.common.util.BaseEntity;
+
+@Entity
+@Table(name="music")
+public class Music extends BaseEntity{
+
+}
